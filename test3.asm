edce7d3bd21948e26a5b2c4e2aaf6428c382dfd9
juaaniolave <juaaniolave@gmail.com>
2023-06-20 19:07:23 -0300

    fin

--- test3.asm
+++ test3.asm
@@ -1,18 +1,3 @@
-main:               mov [ds], 10
-                    mov ah, 3
-                    mov al, 1
-                    mov ch, 1
-                    mov cl, 1
-                    mov dh, 1
-                    mov dl, 1
-                    mov ebx, ds
-                    sys 13
-                    mov ah, 2
-                    mov ebx, es
-                    sys 13
-                    mov edx, es
-                    mov cl, 4
-                    mov ch, 4
-                    mov al, 1
-                    sys 2
-                    stop
+main:       mov ax, 1
+            mov cx, 400
+            mov ebx
